4e4d641fbd210ce53c0ece1249e495a5531271a3
Cornelius Smith <nealsmithg@gmail.com>
2021-08-31 17:06:47 -0400

    added buttons to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,22 @@ var mainPage = document.getElementById("mainbody");
 var secondsLeft = 45;
 var quizEl = document.getElementById("quiz");
 var questions = [
-    question1 = "blah"
+    {
+    question: "Who invented Javascript",
+    answers: {
+        a: "d",
+        b: "s",
+        c: "b"
+    },
+    correctAnswer: "c"
+    }
 ];
+
 
 startbutton.onclick = function(){
     mainPage.style.display = "none";
     timer();
-
+    getQuestion();
 };
 
 function timer(){
@@ -30,5 +39,25 @@ function timer(){
 };
 
 function getQuestion(){
-    var curentQuestion
+    var curentQuestion = questions[0].question;
+    quizEl.textContent = curentQuestion;
+    var answerA = document.createElement("button");
+    answerA.textContent = "A:" + questions[0].answers["a"];
+    answerA.setAttribute("data-letter", "a");
+    answerA.onclick = checkAnswer("a");
+    quizEl.appendChild(answerA);
+    var answerB = document.createElement("button");
+    answerB.textContent = "B:" + questions[0].answers["b"];
+    answerB.setAttribute("data-letter", "b");
+    quizEl.appendChild(answerB);
+    var answerC = document.createElement("button");
+    answerC.textContent = "C:" + questions[0].answers["c"];
+    answerC.setAttribute("data-letter", "c");
+    quizEl.appendChild(answerC);
+    var answerD = document.createElement("button");
+    answerD.textContent = "D:" + questions[0].answers["d"];
+    answerD.setAttribute("data-letter", "d");
+    quizEl.appendChild(answerD);
+    console.log(curentQuestion, questions);
 }
+
